8cbe748319906ff5eaf05cfb427a1c2cc947a352
Saúl Romero Cruz <saulromero200323@gmail.com>
2026-09-14 23:47:57 -0600

    chore(quality): configure linting and git hooks

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt({
+  rules: {
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
+})
