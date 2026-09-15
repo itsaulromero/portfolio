afc3e225d8a8cc31ea68fe14a705d49617d32bad
Saúl Romero Cruz <saulromero200323@gmail.com>
2026-09-15 11:11:22 -0600

    feat(i18n): configure spanish and english localization

---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { t } = useI18n()
+
+useSeoMeta({
+  title: () => t('seo.home.title'),
+  description: () => t('seo.home.description'),
+})
+</script>
+
+<template>
+  <main>
+    <h1>{{ t('home.status.title') }}</h1>
+    <p>{{ t('home.status.description') }}</p>
+  </main>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,36 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss'],
+
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
+
   tailwindcss: {
     cssPath: './app/assets/css/tailwind.css',
+  },
+
+  i18n: {
+    defaultLocale: 'es',
+    strategy: 'prefix_except_default',
+    langDir: 'locales',
+    locales: [
+      {
+        code: 'es',
+        name: 'Español',
+        language: 'es-MX',
+        file: 'es.json',
+      },
+      {
+        code: 'en',
+        name: 'English',
+        language: 'en-US',
+        file: 'en.json',
+      },
+    ],
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root',
+      fallbackLocale: 'es',
+    },
   },
 })
