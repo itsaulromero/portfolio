2d3f70dd2477fe792a4e304ef82d13689e400c92
Saúl Romero Cruz <saulromero200323@gmail.com>
2026-09-18 03:06:59 -0600

    chore(deploy): configure cloudflare workers preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  nitro: {
+    preset: 'cloudflare_module',
+  },
+
   css: ['@fontsource-variable/roboto/wght.css'],
 
   runtimeConfig: {
